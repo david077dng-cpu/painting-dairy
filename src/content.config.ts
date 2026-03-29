b18bd4caaea0e23cb278067696da1e05a6cda69a
david <david077dng@gmail.com>
2026-03-29 21:31:35 +0800

    show group

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,8 +1,6 @@
 import { defineCollection, z } from 'astro:content';
-import { glob } from 'astro/loaders';
 
 const posts = defineCollection({
-  loader: glob({ pattern: '**/*.md', base: './src/content/posts' }),
   schema: z.object({
     title: z.string(),
     date: z.union([z.string(), z.date()]).transform((val) =>
@@ -13,15 +11,4 @@ const posts = defineCollection({
   }),
 });
 
-const groups = defineCollection({
-  loader: glob({ pattern: '**/*.md', base: './src/content/group' }),
-  schema: z.object({
-    title: z.string(),
-    date: z.union([z.string(), z.date()]).transform((val) =>
-      val instanceof Date ? val.toISOString().split('T')[0] : val
-    ),
-    description: z.string().optional(),
-  }),
-});
-
-export const collections = { posts, groups };
+export const collections = { posts };
